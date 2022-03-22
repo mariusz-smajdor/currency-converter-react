af1d53bc338624cc9c8b03ad9c76b44aa119dcf3
Mariusz <smajdormariusz98@gmail.com>
2022-03-22 15:40:52 +0100

    currencies info

---
+++ src/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+  {
+    name: "Euro",
+    short: "EUR",
+    rate: 4.8013
+  },
+  {
+    name: "Dolar amerykański",
+    short: "USD",
+    rate: 4.3482
+  },
+  {
+    name: "Funt brytyjski",
+    short: "GBP",
+    rate: 5.7264
+  }
+]
