a9e8980502ea0288dfdeb80d7b8f9c9605dec304
Mariusz <smajdormariusz98@gmail.com>
2022-03-22 15:49:51 +0100

    controlling input values with useState

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,12 @@
+import { useState } from "react"
+
+import { currencies } from "../../currencies"
 import "./style.css"
 
 const Form = () => {
+  const [amount, setAmount] = useState("")
+  const [currency, setCurrency] = useState("EUR")
+
   return (
     <form className="form">
       <h1 className="form__header">Przelicznik walut</h1>
@@ -12,14 +18,20 @@ const Form = () => {
           type="number"
           step="0.01"
           required
+          value={amount}
+          onChange={event => setAmount(event.target.value)}
         />
       </label>
       <label className="form__label">
         <span className="form__field-text">Chcę dostać:</span>
-        <select className="form__field">
-          <option value="EUR">Euro</option>
-          <option value="USD">Dolar amerykański</option>
-          <option value="GBP">Funt brytyjski</option>
+        <select
+          className="form__field"
+          value={currency}
+          onChange={event => setCurrency(event.target.value)}
+        >
+          {currencies.map(currency => (
+            <option key={currency.short} value={currency.short}>{currency.name}</option>
+          ))}
         </select>
       </label>
       <button className="form__button" type="submit">Przelicz!</button>
