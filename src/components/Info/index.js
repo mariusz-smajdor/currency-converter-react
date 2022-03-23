090fbaf807e8fd2d20e39839c8afba14bd94c485
Mariusz <smajdormariusz98@gmail.com>
2022-03-23 17:21:23 +0100

    added clock into info component

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,12 +1,38 @@
+import { useState, useEffect } from "react"
 import "./style.css"
 
+const formatDate = date => date.toLocaleString(undefined, {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit"
+})
+
 const Info = props => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const dateInterval = setInterval(() => {
+      setDate(new Date())
+    }, 1_000)
+
+    return () => {
+      clearInterval(dateInterval)
+    }
+  }, [])
+
   return (
     <section className="info">
       <p className="info__result">{props.result && `${props.result.sourceAmount} PLN = ${props.result.targetAmount} ${props.result.currency}`}</p>
       <p className="info__source">
         Kursy walut pochodzą ze strony nbp.pl z tabeli nr
         048/A/NBP/2022 z dnia 2022 03 10.
+      </p>
+      <p className="info__date">
+        Dzisiaj jest {" "}
+        {formatDate(date)}
       </p>
     </section>
   )
