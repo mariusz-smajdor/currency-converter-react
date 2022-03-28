dbe25749448d336072afdc9cda357da7057c0184
Mariusz <smajdormariusz98@gmail.com>
2022-03-28 02:50:51 +0200

    applied global styles and themes to the dom render function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from "./theme"
+import { GlobalStyles } from "./GlobalStyles"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
